02da6c84fe6a4b1d9450a12b40092ad9e33ce621
ruslana <ruslana.prus@ukr.net>
2024-12-08 17:49:12 +0100

    user not found exception

--- src/main/java/com/example/notemanager/config/SecurityConfig.java
+++ src/main/java/com/example/notemanager/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.notemanager.config;
 
+import com.example.notemanager.exception.EntityException;
+import com.example.notemanager.exception.ExceptionMessages;
 import com.example.notemanager.model.User;
 import com.example.notemanager.service.UserService;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -45,6 +48,9 @@ public class SecurityConfig {
     public UserDetailsService userDetailsService(UserService userService) {
         return username -> {
             User user = userService.findByUserName(username);
+            if (user == null) {
+                throw new EntityException(ExceptionMessages.USER_NOT_FOUND.getMessage());
+            }
             return org.springframework.security.core.userdetails.User
                     .withUsername(user.getUserName())
                     .password(user.getPassword())
--- src/main/java/com/example/notemanager/exception/ExceptionMessages.java
+++ src/main/java/com/example/notemanager/exception/ExceptionMessages.java
@@ -5,7 +5,8 @@ public enum ExceptionMessages {
     NOTE_ALREADY_EXISTS("Note with this ID already exists"),
     INVALID_NOTE_DATA("Invalid note data provided"),
     INVALID_NOTE_ID("Invalid note id provided"),
-    ENTITY_NOT_FOUND("Such entity wasn't found");
+    ENTITY_NOT_FOUND("Such entity wasn't found"),
+    USER_NOT_FOUND("Such user wasn't found"),;
 
     private String message;
 
